82d7646ca8afb0fe6f66724f1ef555baf27bf030
inandexhale95 <inandexhale95@gmail.com>
2022-05-30 01:07:06 +0900

    register setting

--- src/main/java/com/devlog/piano/security/controller/SecurityController.java
+++ src/main/java/com/devlog/piano/security/controller/SecurityController.java
@@ -1,13 +1,39 @@
 package com.devlog.piano.security.controller;
 
+import com.devlog.piano.member.domain.Member;
+import com.devlog.piano.member.domain.Role;
+import com.devlog.piano.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SecurityController {
 
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public SecurityController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @GetMapping("/system/login")
     public void login() {}
+
+    @GetMapping("/system/register")
+    public String register() {
+        return "/system/register";
+    }
+
+    @PostMapping("/system/register")
+    public String register(Member member) {
+        member.setEnabled(true);
+        member.setRole(Role.ROLE_MEMBER);
+        memberRepository.save(member);
+
+        return "redirect:system/login";
+    }
 
     @GetMapping("/system/accessDenied")
     public void accessDenied() {}
